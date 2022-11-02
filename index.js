bb8a8c9f6a7adf53b7bdfb0aa85147102601021f
xche486 <xche486@uwo.ca>
2022-11-02 18:24:18 -0400

    rewrite backend frontend functions , use postgresdb

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {Router} = require("express");
 const express = require("express");
-const mysql = require('mysql');
-const cors = require('cors')
+const { Pool, Client } = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const router = express.Router();
@@ -20,69 +21,97 @@ app.use((req,res,next)=>{ // for all routes
 //router 
 app.use('/api/', router);//install router at this location
 
-//database connection  
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Axy2436.",
-    database: "lab3"
+app.use(bodyParser.json());
+
+const con = new Pool({
+    user: 'postgres',
+    host: 'db.qlcnnpcecwjoarqzautb.supabase.co',
+    database: 'postgres',
+    password: 'hp0Li1VwztVOl6AW',
+    port: 5432,
+  })
+
+//check db connection 
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
+
 
 //get all available genre names, IDs and parent IDs
 router.get('/genres',(req, res)=> {
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
     condition ="SELECT title as names, genre_id as IDs, parent as Parent_IDs FROM genres" 
     con.query(condition , function (err , genres, fields) {
       if (err) throw err;
-      //console.log(result)
-      res.send(genres);
-    });
+      res.send(genres.rows);
   }); 
 });
 
 
 //get specific artist information 
-router.get('/artists/:artist_id',(req, res)=> {
-    con.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
-      condition ="SELECT artist_id, artist_comments, artist_favorites, artist_handle, artist_name,artist_url, tags FROM raw_artists" ;
-      con.query(condition , function (err , artists, fields) {
+app.post('/api/artists',(req, res)=> {
+    const id = req.body.ID;
+    condition ="SELECT artist_id, artist_comments, artist_favorites, artist_handle, artist_name,artist_url, tags FROM raw_artists WHERE artist_id = '"+ id + "' " ;
+    con.query(condition , function (err , artists, fields) {
         if (err) throw err;
-        //console.log(result)
-        const id = req.params.artist_id;
-        const artist = artists.find( a => a.artist_id === parseInt(id));
-        if (artist){
-            res.send(artist);
+        if (artists){
+            res.send(artists.rows);
         }
         else {
             res.status(404).send(`GET requests for ${id} cannot be found`)
-        };
-      });
-    }); 
+        }; 
+    });
 });
 
 //get following details for a given track id
-router.get('/artists/:artist_id',(req, res)=> {
-    con.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
-      condition ="SELECT artist_id, artist_comments, artist_favorites, artist_handle, artist_name,artist_url, tags FROM raw_artists" ;
-      con.query(condition , function (err , artists, fields) {
+app.post('/tracks',(req, res)=> {
+    const id = req.body.ID; 
+    condition ="SELECT album_id, album_title, artist_id, artist_name, tags, track_date_created, track_date_recorded, track_duration, track_genres, track_number, track_title  FROM raw_tracks WHERE track_id= " + id + " " ;
+      con.query(condition , function (err , track, fields) {
+     console.log(track)
+       if (track.rows){
+            //delete track.rows.track_id
+            res.send(track.rows);
+        }
+        else {
+            res.status(404).send(`GET requests for ${id} cannot be found`)
+        };
+    }); 
+});
+
+
+//get track id based on pattern with album and track title
+app.post('/tracks_pattern',(req, res)=> {
+    const title = req.body.title; 
+    const pattern = "%"+ title + "%";
+    const n = req.body.n; 
+
+    condition ="SELECT track_id FROM raw_tracks WHERE track_title LIKE '"+ pattern + "' OR album_title LIKE '" + pattern + "'LIMIT '" + n+ "'";
+        con.query(condition , function (err , tracks, fields) {
         if (err) throw err;
-        //console.log(result)
-        const id = req.params.artist_id;
-        const artist = artists.find( a => a.artist_id === parseInt(id));
-        if (artist){
-            res.send(artist);
+         if (tracks){
+            res.send(tracks.rows);
+        }
+        else {
+            res.status(404).send(`GET requests for ${id} cannot be found`)
+        };
+    });
+});
+
+//get artist ids based on pattern with name 
+app.post('/artist_pattern',(req, res)=> {
+        const name = req.body.pattern;
+        const pattern = "%"+name+"%";
+        condition ="SELECT artist_id, artist_name FROM raw_artists WHERE artist_name LIKE '"+ pattern + "' ";
+        con.query(condition , function (err , artists, fields) {
+        if (err) throw err;
+         if (artists.rows){
+            res.send(artists.rows);
         }
         else {
             res.status(404).send(`GET requests for ${id} cannot be found`)
         };
       });
-    }); 
 });
 
 
--- static/front.js
+++ static/front.js
@@ -3,30 +3,43 @@ function allgenres(){
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        data2=data;
+        /*
+        let i = 0;
+        result = document.getElementById("all_genres")
+        while (i < data.length){
+          genre = document.createElement('div');
+          genre.setAttribute("class", "genre");
+          genre
+
+          genrename = document.createElement('p');
+          gname=document.createTextNode(data2[i].names);
+          genrename.appendChild(gname);
+          genre.appendChild(genrename);
+
+
+          genreid = document.createElement('p');
+          gid=document.createTextNode(data2[i].IDs)
+          genreid.appendChild(gid);
+          genre.appendChild(genreid);
+
+          genreparent = document.createElement('p');
+          gparent=document.createTextNode(data2[i].parents)
+          genreparent.appendChild(gparent);
+          genre.appendChild(genreparent);
+        }
         document.getElementById("1").innerHTML = data2[0].names;
         document.getElementById("2").innerHTML = data2[0].IDs;
         document.getElementById("3").innerHTML = data2[0].parents;
         document.getElementById("4").innerHTML = data2[1].names;
+
       // do something with data
+      */
+     
     })
     .catch(rejected => {
     console.log(rejected);
-});
-       
-
-genre = document.createElement('div');
-genre.setAttribute("class", "genre");
-genrename = document.createElement('p');
-genreid = document.createElement('p');
-genreparent = document.createElement('p');
-genre.appendChild(genrename);
-genre.appendChild(genreid);
-genre.appendChild(genreparent);
-
-// may need to createTextNode as well
-
-//create artist 
-artistIDfind(){
+  });
 
 }
+       
+
